77d5c67e9e566df590c6704f79ecc61a97a51207
tochmeanrith-blip <toch.meanrith@gmail.com>
2026-07-06 00:58:17 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -155,10 +155,10 @@ def webhook():
             reminder_str = format_khmer_date(reminder_date)
             
             bot.send_message(update.message.chat_id,
-                             f""" បានកត់ត្រាជោគជ័យ!
+                             " បានកត់ត្រាជោគជ័យ!
                              📅 ថ្ងៃព្រឹត្តិការណ៍: {event_str}
                              🔔 ថ្ងៃរំលឹក: {reminder_str}
-                             """)
+                             ")
         else:
             bot.send_message(update.message.chat_id,
                              "❌ សូមសរសេរថ្ងៃជាទម្រង់: 15 ខែ សីហា 2026")
